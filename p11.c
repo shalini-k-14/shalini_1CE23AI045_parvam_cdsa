63647d621dee73634c1d5b3c3a947706d6bf9854
shalini-k-14 <shalinik3964@gmail.com>
2024-11-11 17:22:09 +0530

    Delete p11.c

--- p11.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if (num >=10 && num <=99){
-        printf("The given number is 2 digit number.");
-    }
-}
